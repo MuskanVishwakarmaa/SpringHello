271bcc8de5f68898da72be5040da1bbf9b53172b
MuskanVishwakarmaa <muskanvishwakarma06941@gmail.com>
2025-03-01 23:27:52 +0530

    Hello using Get method(UC1)

---
+++ src/main/java/com/example/Springhello/SpringhelloApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Springhello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringhelloApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringhelloApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Springhello/SpringhelloApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Springhello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringhelloApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
